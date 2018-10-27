ecb60edbd01fb98df4b6a43e802850d0c7cfc270
lijian.yin <lijian.yin@hand-china.com>
2018-10-27 10:20:07 +0800

    [IMP] 组合模式

---
+++ src/main/java/com/yin/design/Composite.java
@@ -0,0 +1,55 @@
+package com.yin.design;
+
+import lombok.Data;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+/**
+ *  结构型模式 - 组合模式
+ *
+ * @author lijian.yin@hand-china.com
+ * @date 2018/10/27
+ */
+public class Composite {
+    public static void main(String[] args) {
+        Tree tree = new Tree("A");
+        TreeNode nodeB = new TreeNode("B");
+        TreeNode nodeC = new TreeNode("C");
+
+        nodeB.add(nodeC);
+        tree.root.add(nodeB);
+        System.out.println("build the tree finished!");
+    }
+}
+class Tree {
+    TreeNode root = null;
+
+    public Tree(String name) {
+        root = new TreeNode(name);
+    }
+}
+
+@Data
+class TreeNode {
+
+    private String name;
+    private TreeNode parent;
+    private Vector<TreeNode> children = new Vector<TreeNode>();
+
+    public TreeNode(String name){
+        this.name = name;
+    }
+
+    public void add(TreeNode node){
+        children.add(node);
+    }
+
+    public void remove(TreeNode node){
+        children.remove(node);
+    }
+
+    public Enumeration<TreeNode> getChildren(){
+        return children.elements();
+    }
+}
